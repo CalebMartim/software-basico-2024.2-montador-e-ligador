5980e7347f46abc4d8da3bc229463b8d9fed1c62
calebmartim <calebmartim2113@gmail.com>
2025-01-14 15:59:27 -0300

    Adicionado detalhes

--- MONTADOR.cpp
+++ MONTADOR.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 
 /* A diretiva a seguir é feita para que não seja necessário 
 insrir o prefixo "std::" em toda instância de um
@@ -12,7 +13,9 @@ using namespace std;
 #warning falta implementar macros e outras diretivas
 #warning falta implementar operadores nas labels
 #warning corrigir erros de números de operandos para cada token que contém comentário
-#warning tratar quando diretivas são realizadas em text ou data
+#warning falta tratar diferente o arquivo dependendo se ele é um módulo ou não
+#warning por enquanto perdemos quando valores de operacoes são hexadecimais, vamos fazer isso no pré-processamento
+#warning mudar strings nos erros para constantes
 
 unordered_map<string, int> tamanho_da_instrucao = {
   {"ADD", 2},
@@ -48,6 +51,10 @@ unordered_map<string, string> opcode_da_instrucao = {
   {"STOP", "14"}
 };
 
+unordered_set<string> diretivas_para_ignorar = {
+  "BEGIN", "END", "SECTION"
+};
+
 // -- Funções auxiliares, início:
 vector<string> pegue_tokens(const string & s){
   vector<string> ret;
@@ -69,8 +76,7 @@ vector<string> pegue_tokens(const string & s){
 }
 
 void remove_comentarios(vector<string> & tokens) {
-  int quantidade = (int) tokens.size();
-  int comeca_comentario = -1;
+  int quantidade = (int) tokens.size(), comeca_comentario = -1;
   for (int i = 0; i < quantidade and comeca_comentario == -1; ++i) 
     if (tokens[i].front() == ';')
       comeca_comentario = i;
@@ -80,13 +86,14 @@ void remove_comentarios(vector<string> & tokens) {
 }
 
 int hexa_para_decimal(const char & c) {
-  /* Converte um dígito hexadecimal para seu valor decimal*/
-   if (c >= '0' and c <= '9') return c - '0';
-   return c - 'A' + 10;
+  /* Converte um dígito hexadecimal para seu valor decimal. 
+  É assumido que o caractére passado é, de fato, um dígito hexadecimal. */
+  if (c >= '0' and c <= '9') return c - '0';
+  return c - 'A' + 10;
 }
 
 string hexa_para_binario(const char & c) {
-  /* Converte um dígito hexadecimal para seu valor em binario*/
+  /* Converte um dígito hexadecimal para seu valor em binario de 4 bits.*/
   int valor = hexa_para_decimal(c);
   string valor_binario(4, '0');
   for (int i = 0; i < 4; ++i)
@@ -98,7 +105,9 @@ string hexa_para_binario(const char & c) {
 
 string hexa_para_decimal(const string & s) {
   /* Converte um valor hexadecimal em complemento de dois 
-  para o seu valor hexadecimal */
+  para o seu valor decimal. É assumido que o valor passado
+  é um valor hexadecimal válido que pode ser representado 
+  por um int. */
   string digitos_em_binario;
   for (int i = 2; i < (int) s.length(); ++i) 
     digitos_em_binario += hexa_para_binario(s[i]);
@@ -149,32 +158,25 @@ int main(int argc, char* argv[]){
     nome_do_arquivo += arquivo_recebido[i];
 
   /* Processo de leitura do arquivo. */
-  ifstream arquivo(argv[1]);
+  ifstream stream_do_arquivo(arquivo_recebido);
   vector<string> linhas_do_programa;
   string aux;
-  while (getline(arquivo, aux)) 
+  while (getline(stream_do_arquivo, aux)) 
     linhas_do_programa.push_back(aux);    
-  arquivo.close(); 
+  stream_do_arquivo.close(); 
 
   /* Tendo lido nosso arquivo, vamos construir os tokens dos comandos,
-  remover seus comentários e garantir que a ordem
-  da seção TEXT e DATA esteja correta */
+  vamos considerar quando o programa se trata de um módulo a 
+  ser ligado ou não. Assim como de acordo com a especificação,
+  comentários sào removidos nas diretivas IF e EQU */
   vector<vector<string>> tokens_data, tokens_text;
-  bool lendo_text = false, lendo_data = false, pula_instrucao = false;
-  unordered_map<string, string> sinonimo;
-  auto valor = [&sinonimo](const string & s) -> string {
-    if ((s.length() >= 2 and s.substr(0, 2) == "0X"))
-      return hexa_para_decimal(s);
-    if (sinonimo.count(s)) 
-      return sinonimo[s];
-    return s;
-  };
+  bool lendo_text = true, representa_modulo = false;
   for (const string & linha : linhas_do_programa) {
     vector<string> tokens = pegue_tokens(linha);
-    if (tokens.empty()) continue; // Linha vazia ou composta apenas por comentário
-    if (tokens[0] == "SECTION")
+    if (tokens.empty()) continue; // Linha vazia
+    else if (tokens[0] == "SECTION")
       lendo_text = (tokens[1] == "TEXT");
-    if (tokens[0] == "IF" or ((int) tokens.size() > 1 and tokens[1] == "EQU"))
+    else if (tokens[0] == "IF" or ((int) tokens.size() > 1 and tokens[1] == "EQU"))
       remove_comentarios(tokens); 
     if (lendo_text)
       tokens_text.push_back(tokens);
@@ -191,37 +193,51 @@ int main(int argc, char* argv[]){
       if (operacao[0] == "COPY") {
         saida << operacao[0];
         if ((int) operacao.size() > 1) saida << ' '; 
-        for (int i = 1; i < (int) operacao.size(); ++i) 
-          saida << operacao[1] << ",\n"[i == (int) operacao.size() - 1];
+        bool comentario = false;
+        for (int i = 1; i < (int) operacao.size(); ++i) {
+          if (operacao[i] == ";") comentario = true;
+          if (comentario)
+            saida << operacao[i] << " \n"[i == (int) operacao.size() - 1];
+          else 
+            saida << operacao[i] << ",\n"[i == (int) operacao.size() - 1];
+        } 
       } else 
           for (int i = 0; i < (int) operacao.size(); ++i) 
-            saida << valor(operacao[i]) << " \n"[i == (int) operacao.size() - 1];
+            saida << operacao[i] << " \n"[i == (int) operacao.size() - 1];
     for (const vector<string> & operacao : tokens_data) 
       for (int i = 0; i < (int) operacao.size(); ++i) 
-        saida << valor(operacao[i]) << " \n"[i == (int) operacao.size() - 1];
-      saida.close();
+        saida << operacao[i] << " \n"[i == (int) operacao.size() - 1];
+    saida.close();
   } else {
 
     // Montagem do arquivo //
 
-    /* Primeira passagem: */
-    unordered_map<string, string> tabela_de_simbolos;
+    /* Primeira passagem e verificação de alguns erros sintáticos: */
+    unordered_map<string, string> tabela_de_simbolos, tabela_de_sinonimos;
     int contador_de_instrucao = 0, contador_de_linha = 1;
     for (const vector<string> & operacao : tokens_text) {
-      if (operacao[0] == "SECTION") continue;
-      if (operacao[0].back() == ':') { // É a definição de uma rotulo
+      if (diretivas_para_ignorar.count(operacao[0])) continue;
+      if (operacao[0].back() == ':') { // É a definição de um rótulo
         string rotulo = operacao[0]; 
+        rotulo.pop_back(); // Remove o ':' no fim 
         if (rotulo_invalido(rotulo)) {
           erro(contador_de_linha, "rotulo_invalido");
           return 1;
         }  
-        rotulo.pop_back(); // Remove o ':'
-        if (tabela_de_simbolos.count(rotulo) > 0) { // Verifica se já não foi definido
+        if (tabela_de_simbolos.count(rotulo)) { // Verifica se já não foi definido
           erro(contador_de_linha, "rotulo_redefinido");
           return 1;
         } 
+        if ((int) operacao.size() > 1 and operacao[1] == "EQU") {
+          // O rótulo é um símbolo para substituir em certos locais
+          if ((int) operacao.size() <= 2) {
+            erro(contador_de_linha,"expressao_invalida");
+            return 1;
+          } else 
+            tabela_de_sinonimos[rotulo] = operacao[2];
+        }
         tabela_de_simbolos[rotulo] = to_string(contador_de_instrucao);
-        if (operacao.size() > 1) {
+        if ((int) operacao.size() > 1) {
           if (operacao[1].back() == ':') { // Verifica se há uma definição duplicada aqui
             erro(contador_de_linha, "rotulo_dobrado");
             return 1; 
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# software-basico-2024.2-montador-e-ligador
+# Software Básico 2024.2: Montador e Ligador
+
+Autor: Caleb Martim 
+
+Descrição: Um montador e um ligador para o "Assembly inventado" da disciplina.
+
+<!-- Sistema Operacional: Arch Linux, Kernel: 6.12.6-arch1-1 -->
+
+Como compilar:
+```g++ -std=c++17 montador.cpp -o montador```
+```g++ -std=c++17 ligador.cpp -o ligador```
+
+Como usar:
+```./montador a.asm``` 
+```./montador a.pre```
+```./ligador a.obj b.obj```
+
+## Considerações:
+
+Estamos assumindo as seguintes condições:
+
+1) O usuário sempre passará como argumento no montador um arquivo .pre ou um
+arquivo .asm
+
+2) Estamos assumindo que valores inteiros passados nas diretivas CONST e EQU
+podem ser representados por um valor do tipo int, isto é, ele pertence ao intervalo 
+[-2^{31}, 2^{31} - 1]
+
+3) Estamos assumindo, pelo menos por agora, que erros sintáticos, semânticos não são realizados em diretivas IF e EQU
+
+4) Se for verificado algum erro sintático, a montagem para imediatamnete.
+
+5) Estamos assumindo que o arquivo .pre corretamente divide o programa em SECTION TEXT e SECTION DATA
